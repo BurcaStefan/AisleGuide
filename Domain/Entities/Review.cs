1e0f2c22ee6a7865ef84786c8c436f49a1abcdcb
BurcaStefan <burcaiulianstefan@gmail.com>
2025-03-29 18:47:37 +0200

    accept review content as null

--- Domain/Entities/Review.cs
+++ Domain/Entities/Review.cs
@@ -11,7 +11,7 @@ namespace Domain.Entities
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public Guid ProductId { get; set; }
-        public string Content { get; set; }
+        public string? Content { get; set; }
         public int Rating { get; set; }
         public DateTime CreatedAt { get; set; }
     }
