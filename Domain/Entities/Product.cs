d4b9d74259cbd2252fc1c7fa7d3324127a26b45b
BurcaStefan <burcaiulianstefan@gmail.com>
2025-03-29 18:04:21 +0200

    Product entity

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Entities
+{
+    class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string ISBN { get; set; }
+        public string? ShelvingUnit { get; set; }
+        public float? AverageRating { get; set; }
+
+        // Nutritional information
+        public int Calories { get; set; }
+        public float Protein { get; set; }
+        public float Carbohydrates { get; set; }
+        public float Sugars { get; set; }
+        public float Fat { get; set; }
+        public float SaturatedFat { get; set; }
+        public float Fiber { get; set; }
+        public float Salt { get; set; }
+        public float Cholesterol { get; set; }
+    }
+}
