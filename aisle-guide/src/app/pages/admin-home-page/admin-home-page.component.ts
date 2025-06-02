c7b12b9fd6c642351373abf3dd1652185c52a035
BurcaStefan <burcaiulianstefan@gmail.com>
2025-06-02 09:39:39 +0300

    create headers and footers

--- aisle-guide/src/app/pages/admin-home-page/admin-home-page.component.ts
+++ aisle-guide/src/app/pages/admin-home-page/admin-home-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { AdminHeaderComponent } from '../../components/layout/admin-header/admin-header.component';
 
 @Component({
   selector: 'app-admin-home-page',
-  imports: [],
+  imports: [AdminHeaderComponent],
   templateUrl: './admin-home-page.component.html',
-  styleUrl: './admin-home-page.component.scss'
+  styleUrl: './admin-home-page.component.scss',
 })
-export class AdminHomePageComponent {
-
-}
+export class AdminHomePageComponent {}
