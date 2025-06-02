0aada4c1eafb09c70ae26b75fd3ba445e1b1e306
BurcaStefan <burcaiulianstefan@gmail.com>
2025-06-02 17:46:41 +0300

    admin home page

--- aisle-guide/src/app/pages/admin-home-page/admin-home-page.component.ts
+++ aisle-guide/src/app/pages/admin-home-page/admin-home-page.component.ts
@@ -8,4 +8,22 @@ import { AdminFooterComponent } from '../../components/layout/admin-footer/admin
   templateUrl: './admin-home-page.component.html',
   styleUrl: './admin-home-page.component.scss',
 })
-export class AdminHomePageComponent {}
+export class AdminHomePageComponent {
+  rowIndices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  colIndices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  gridData = [
+    ['', '', 'O1', 'O2', 'O3', 'O4', 'O5', 'M1', 'M2', 'M3', 'M4', 'M5', ''],
+    ['', '', '', '', '', '', '', '', '', '', '', '', 'C5'],
+    ['', '', 'G1', 'G2', 'G3', 'G4', '', 'M5', 'M6', 'M7', 'M8', '', 'C4'],
+    ['', '', 'N1', 'N2', 'N3', 'N4', '', 'A1', 'A2', 'A3', 'A4', '', 'C3'],
+    ['', '', '', '', '', '', '', '', '', '', '', '', 'C2'],
+    ['', '', 'N5', 'N6', 'N7', 'N8', '', 'A5', 'A6', 'A7', 'A8', '', 'C1'],
+    ['', '', 'H1', 'H2', 'H3', 'H4', '', 'I1', 'I2', 'I3', 'I4', '', 'D5'],
+    ['', '', '', '', '', '', '', '', '', '', '', '', 'D4'],
+    ['', '', 'F1', 'F2', 'F3', 'F4', '', 'V1', 'V2', 'V3', 'V4', '', 'D3'],
+    ['', '', 'F5', 'F6', 'F7', 'F8', '', 'V5', 'V6', 'V7', 'V8', '', 'D2'],
+    ['➔', '', '', '', '', '', '', '', '', '', '', '', 'D1'],
+    ['', '', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'B1', 'B2', 'B3', ''],
+  ];
+}
