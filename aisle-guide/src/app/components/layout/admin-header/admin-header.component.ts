09d9543a4a65512ebe5181a2bb7c1cbeef0563c5
BurcaStefan <burcaiulianstefan@gmail.com>
2025-06-02 11:10:28 +0300

    header admin

--- aisle-guide/src/app/components/layout/admin-header/admin-header.component.ts
+++ aisle-guide/src/app/components/layout/admin-header/admin-header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-header',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './admin-header.component.html',
-  styleUrl: './admin-header.component.scss'
+  styleUrl: './admin-header.component.scss',
 })
 export class AdminHeaderComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
