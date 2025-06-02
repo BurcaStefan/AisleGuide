ff2afb3dbafd5ce6f8c47d45a55193f4d3ffe507
BurcaStefan <burcaiulianstefan@gmail.com>
2025-06-02 12:13:41 +0300

    admin footer

--- aisle-guide/src/app/pages/admin-home-page/admin-home-page.component.ts
+++ aisle-guide/src/app/pages/admin-home-page/admin-home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AdminHeaderComponent } from '../../components/layout/admin-header/admin-header.component';
+import { AdminFooterComponent } from '../../components/layout/admin-footer/admin-footer.component';
 
 @Component({
   selector: 'app-admin-home-page',
-  imports: [AdminHeaderComponent],
+  imports: [AdminHeaderComponent, AdminFooterComponent],
   templateUrl: './admin-home-page.component.html',
   styleUrl: './admin-home-page.component.scss',
 })
